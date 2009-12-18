faec112592bf61d3952946c6160f6e400c32930e
Trevor Chapman <chapmant@onid.oregonstate.edu>
2009-12-17 23:08:08 -0800

    starting gem.py object

---
+++ bejeweled/gem.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import pygame, sys, random
+
+class Gem:
+	column = 0
+	row = 0
+	def __init__(self, image, gem_rect):
+		self.gem = image
+	def set_location(board_col, board_row):
+		column = board_col
+		row = board_row
+	def blit():
+		screen.blit(self.gem, self.gem.get_rect())
