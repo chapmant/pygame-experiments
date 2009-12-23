b8ab27989d75be0d825be56a81c4effd2415e94f
chapmant <chapmant@onid.oregonstate.edu>
2009-12-23 10:59:46 -0800

    making sure everything in place

--- bejeweled/board.py
+++ bejeweled/board.py
@@ -5,10 +5,15 @@ from gem import Gem
 
 class GameBoard:
 	def __init__(self, rows, columns):
-		height = rows * 25
-		width = columns * 25
-		self.size = height, width
+		pygame.init()
+		self.height = rows * 25
+		self.width = columns * 25
+		self.size = self.height, self.width
 		self.set_board()
+		print self.size
+		print self.height, self.width
 	def set_board(self):
 		self.board = pygame.Surface(self.size)
-		self.board.fill((255,255,255))
+		self.board.fill((123,123,123))
+		rect = self.size
+		print rect
--- bejeweled/game.py
+++ bejeweled/game.py
@@ -4,16 +4,18 @@ import pygame, sys
 from board import GameBoard
 
 class Game:
-	black = (0, 0, 0)
-	board = GameBoard(0,0)
+	black = (100,100,100)
+	board = GameBoard(10,10)
 	def __init__(self):
 		pygame.init()
 		size = width, height = 800, 600
 		self.screen = pygame.display.set_mode(size)
 		self.game_main()
-		self.board = GameBoard(10, 10)
+		#self.board = GameBoard(10, 10)
+		self.screen.blit(self.board.board, self.board.board.get_rect())
 	def game_main(self):
 		print "Test"
+		print self.board.board.get_rect()
 		while 1:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
