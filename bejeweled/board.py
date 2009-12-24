6fa211d96b22549a66671a12087dcd287e4ffbb7
Trevor Chapman <chapmant@onid.oregonstate.edu>
2009-12-23 22:10:12 -0800

    board now set

--- bejeweled/board.py
+++ bejeweled/board.py
@@ -1,34 +1,43 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import pygame
+import pygame, random
 from gem import Gem
 
 class GameBoard:
 	gems = []
-	def __init__(self, rows, columns):
+	def __init__(self, rows, columns, height):
 		pygame.init()
-		self.height = rows * 50
-		self.width = columns * 50
-		self.size = self.height, self.width
-		print self.size
+		self.size = height, height
+		#print self.size
 		self.set_board()
-		print self.size
-		print self.height, self.width
+		#print self.size
+		#print self.height, self.width
 	def set_board(self):
+		# Inits the board, fills it with color!
 		self.board = pygame.Surface(self.size)
 		self.board.fill((123,123,123))
 		rect = self.size
 		print rect
 		self.set_gems()
 	def set_gems(self):
+		# Load the gem images
 		gem_0 = pygame.image.load("gem0.png")
+		gem_1 = pygame.image.load("gem1.png")
+		gem_2 = pygame.image.load("gem2.png")
+		gem_3 = pygame.image.load("gem3.png")
+		gem_list = [gem_0, gem_1, gem_2, gem_3]
+		# Get the rectangle of the gems
 		rect = gem_0.get_rect()
 		i = 0
-		for row in range(0,self.height/25):
-			for column in range(0,self.width/25):
-				self.gems.append(Gem(gem_0, rect))
+		for row in range(0,self.size[0]/25):
+			for column in range(0,self.size[1]/25):
+				# Appends a newly generated Gem to the list of gems
+				self.gems.append(Gem(gem_list[random.randint(0,3)], rect))
+				# Blits the new Gem to the screen.
 				self.board.blit(self.gems[i].gem, self.gems[i].rect)
+				# Shifts the rectangle set by one width.
 				rect.left += 50
 				i += 1
+			# Resets the Gem rectangle to the left, moves down a line.
 			rect.left = 0
 			rect.top += 50
--- bejeweled/game.py
+++ bejeweled/game.py
@@ -1,14 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import pygame, sys
 from board import GameBoard
 
 class Game:
 	black = (100,100,100)
-	board = GameBoard(10,10)
+	game_rect = [0, 0, 300, 200]
+	board = GameBoard(10,10, .9 * game_rect[3])
 	def __init__(self):
 		pygame.init()
-		size = width, height = 800, 600
+		size = width, height = self.game_rect[2], self.game_rect[3]
 		self.screen = pygame.display.set_mode(size)
 		self.game_main()
 		#self.board = GameBoard(10, 10)
@@ -16,6 +17,9 @@ class Game:
 	def game_main(self):
 		print "Test"
 		print self.board.board.get_rect()
+		#game_rect = self.screen.get_rect()
+		board_rect = [.05 * self.game_rect[3], .05 * self.game_rect[3], .05 * self.game_rect[3], .05 * self.game_rect[3]]
+		print board_rect
 		while 1:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
@@ -23,7 +27,7 @@ class Game:
 				elif event.type == pygame.MOUSEBUTTONDOWN:
 					print "Mouse clicked."
 			self.screen.fill(self.black)
-			self.screen.blit(self.board.board, self.board.board.get_rect())
+			self.screen.blit(self.board.board, board_rect)
 			pygame.display.flip()
 
 def main():
