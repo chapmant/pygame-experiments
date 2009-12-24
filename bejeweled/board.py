e3a5131f12e22c27167e99961dffd1d3a50c8794
Trevor Chapman <chapmant@onid.oregonstate.edu>
2009-12-23 18:48:06 -0800

    Changes to board info

--- bejeweled/board.py
+++ bejeweled/board.py
@@ -4,11 +4,13 @@ import pygame
 from gem import Gem
 
 class GameBoard:
+	gems = []
 	def __init__(self, rows, columns):
 		pygame.init()
-		self.height = rows * 25
-		self.width = columns * 25
+		self.height = rows * 50
+		self.width = columns * 50
 		self.size = self.height, self.width
+		print self.size
 		self.set_board()
 		print self.size
 		print self.height, self.width
@@ -17,3 +19,16 @@ class GameBoard:
 		self.board.fill((123,123,123))
 		rect = self.size
 		print rect
+		self.set_gems()
+	def set_gems(self):
+		gem_0 = pygame.image.load("gem0.png")
+		rect = gem_0.get_rect()
+		i = 0
+		for row in range(0,self.height/25):
+			for column in range(0,self.width/25):
+				self.gems.append(Gem(gem_0, rect))
+				self.board.blit(self.gems[i].gem, self.gems[i].rect)
+				rect.left += 50
+				i += 1
+			rect.left = 0
+			rect.top += 50
